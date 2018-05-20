ddf67dc07897f41f78cd1773149d6c70e09a22dd
ffbskt <ffbskt@gmail.com>
2018-05-20 12:22:45 +0300

    solution, but didn't go throu test, cause conditions ambiguosly

--- Find K Closest Elements.py
+++ Find K Closest Elements.py
@@ -13,6 +13,24 @@ def BinSearchNearest(arr, x):
         return start - 1
     return start
 
+def grad(arr, ri, k, x):
+    dl, dr = False, False
+    if len(arr) - 1 > ri:
+        dr = (abs(arr[ri] - x) + abs(x - arr[ri - k]) > 
+              abs(arr[ri + 1] - x) + abs(x - arr[ri + 1 - k]))
+        #print((abs(arr[ri] - x) + abs(x - arr[ri - k]), 
+        #      abs(arr[ri + 1] - x) + abs(x - arr[ri + 1 - k])))
+    elif ri - k > 0:
+        #print(ri, len(arr), k)
+        dl = (abs(arr[ri] - x) + abs(x - arr[ri - k]) > 
+              abs(arr[ri - 1] - x) + abs(x - arr[ri - 1 - k]))
+    else:
+        return False #the arr ended
+    if dr:
+        return 'r'
+    elif dl:
+        return 'l'
+    return False
 
 class Solution(object):
     def findClosestElements(self, arr, k, x):
@@ -22,18 +40,24 @@ class Solution(object):
         :type x: int
         :rtype: List[int]
         """
-        ind = BinSearchNearest(arr, x)
-        if k <= ind:
-            return arr[ind - k:ind]
-        else:
-            return arr[0:k]
+        L, R = 0, len(arr)
+        
+        while L <= R:
+            rind = max((L + R) // 2, k)
+            #rind = max(rind, k)
+            move = grad(arr, rind, k, x)
+            #print('1', move,rind)
+            if move == 'r':
+                L = rind + 1
+            elif move == 'l':
+                R = rind - 1
+            else:
+                return arr[rind-k+1:rind+1]
+
+#print(grad([1,2,3,4,5,8], 4, 4, 5))
     
 
-
-print (BinSearchNearest([1,2,3,4,5,8], 6))
-print (BinSearchNearest([1,2,3,4,5,8], 5))
-print (BinSearchNearest([1,2,3,4,5,8], 8))
 s = Solution()
-print(s.findClosestElements([1,2,3,4,5,8], k=4, x=5))
-print(s.findClosestElements([1,2,3,4,5,8], k=4, x=6))
+print(s.findClosestElements([1,2,3,4,5,8], k=4, x=4))
+#print(s.findClosestElements([1,2,3,4,5,7], k=4, x=6))
     
