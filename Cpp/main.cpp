9e98127fbeb1736735c69bcdf09abb49f8d9b05a
ffbskt <ffbskt@gmail.com>
2018-05-27 08:03:32 +0300

    inorder tree

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -18,122 +18,84 @@ public:
         if (root == NULL) {
             return vec;
         }
-        st.push(root);
+        TreeNode* node = root;
         while (true) {
-            TreeNode* node = st.top();
-            if (node->left != NULL) {
-                st.push(node->left);
-            } else {
-                while (node->right == NULL) {
-                    vec.push_back(node->val);
-                    st.pop();
-                    if (st.empty()) {
-                        return vec;
-                    }   
-                }
+            if (node != NULL) {
+                st.push(node);
+                node = node->left;
+            } else if (!st.empty()) {
+                node = st.top();
+                st.pop(); 
                 vec.push_back(node->val);
-                st.pop();
-                //if (st.empty()) {
-                //    return vec;
-                //}    
-                st.push(node->right);
-            }
+                node = node->right;
+            } else {
+                return vec;      
+            } 
+        
         }
-        return vec;
     }
 };
 
-void inorderTraversal(TreeNode* root) {
-    std::cout << "ss"<< root->val;
+void inorderTraversal(TreeNode* root, std::vector<int>* vec) {
+    //std::cout << "ss"<< root->val;
     if (root->left != NULL) {
-        inorderTraversal(root->left);
+        inorderTraversal(root->left, vec);
     }
-    std::cout << root->val;
+    vec->push_back(root->val);
     if (root->right != NULL) {
-        inorderTraversal(root->right);
+        inorderTraversal(root->right, vec);
     }
 }
 
-std::vector<TreeNode> AddNode(const std::vector<int>& vec, int cur_it, TreeNode* root) {
-    std::cout << cur_it << " " << vec[cur_it];
-    std::vector<TreeNode> tree(0, vec.size());
-    // for (int i; i < vec.size(); ++i) {
+std::vector<int> IT(TreeNode* root) {
+    std::vector<int> vec;
+    inorderTraversal(root, &vec);
+    return vec;
+}
 
-    // }
-    //if (cur_it < vec.size()) {
-    //    if (vec[cur_it] != 0) {
-    //        TreeNode node(vec[cur_it]);
-    //        root->left = &node;
-    //        std::cout << "{" << node.val << "\n";
-    //        AddNode(vec, 2 * cur_it - 1, &node);     
-    //    } else {
-    //        root->left = nullptr;
-    //    }
-    //} else {
-    //     root->left = nullptr;
-    //     root->right = nullptr;
-    // }
-    // if (cur_it < vec.size()) {
-    //     if (vec[cur_it] != 0) {
-    //         TreeNode node(vec[cur_it]);
-    //         root->right = &node;
-    //         std::cout << "{" << node.val << "\n";
-    //         AddNode(vec, 2 * cur_it, &node);     
-    //     } else {
-    //         root->right = nullptr;
-    //     }
-    // }
+std::vector<TreeNode> AddNodes(const std::vector<int>& vec) {
+    std::vector<TreeNode> tree(vec.size(), 0);
+    // std::cout << cur_it << " " << tree.size();
+    
+    for (int i = 0; i < vec.size(); ++i) {
+        tree[i].val = vec[i];
+        if (2 * i + 2 < vec.size() && vec[2 * i + 2] != 0) {
+            std::cout<<"right";
+            tree[i].right = &tree[2 * i + 2];
+        }
+        if (2 * i + 1 < vec.size() && vec[2 * i + 1] != 0) {
+            std::cout<<"left";
+            tree[i].left = &tree[2 * i + 1];
+        }
+        
+    }
     return tree;
 }
 
-struct node {
-    int v = -1;
-    node* u = NULL;
-    ~node() {v = 909; std::cout<<"destroy";}
-};
-
-
-class H {
-  public:
-    void cl(node* root) {
-        node u;
-        u.v = 100;
-        
-        Q.push(u);
-        root->u = &(Q.front());
-        //return k;
-    }
-  std::queue<node> Q;  
-};
 
 
 int main(int argc, char const *argv[])
 {
-    node y;
-    H h;
-    h.cl(&y);
-    std::cout << y.u->v << "\n";
-    //cl(&y);
-    //std::cout << y.u->v << "\n";
+    
+    //int myints[] = {1, 0, 2, 0, 0, 3};
+    std::vector<int> vec{1, 0, 2, 0, 0, 3};
+
+    
+    std::vector<TreeNode> tv = AddNodes(vec);
+    TreeNode* root = &tv[0];
+
+    std::cout << root->val << root->right->val << root->right->left->val<< "\n";
+    std::cout << tv[0].val << tv[1].val << "\n";
+
+    //std::vector<int> v = IT(root);
+
+    Solution s;
+    // std::vector<int> iv{1,2,3};
+    std::vector<int> v = s.inorderTraversal(root);
+    std::cout << "\n Ans \n";
+    for (int i=0; i<v.size(); ++i) {
+        std::cout << v[i] << "\n";
+    }
+    
     return 0;
 }
-//     //int myints[] = {1, 0, 2, 3};
-//     std::vector<int> vec{1, 0, 2, 3};
-
-//     TreeNode root(vec[0]);
-//     AddNode(vec, 1, &root);
-
-//     std::cout << root.val;// << root.right->val << "\n";
-
-//     //inorderTraversal(&root);
-
-//     Solution s;
-//     // std::vector<int> iv{1,2,3};
-//     std::vector<int> v{1}; //= s.inorderTraversal(&root);
-//     std::cout << "\n Ans \n";
-//     for (int i=0; i<v.size(); ++i) {
-//         std::cout << v[i] << "\n";
-//     }
-    
-//     return 0;
-// }
